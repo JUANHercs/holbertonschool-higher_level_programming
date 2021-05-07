aa6283c3c8b866200dc6da165754710433cdf10b
JUANHercs <juan.hercs@gmail.com>
2021-05-07 07:12:46 +0000

    play_with_character_position

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,6 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 str = str[39:66] + str[-23:-17] + str[:6]
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(str)
-
