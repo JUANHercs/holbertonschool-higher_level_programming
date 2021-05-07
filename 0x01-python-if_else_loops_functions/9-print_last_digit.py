96f1efe953978c221b87d0a8b945ee12446ad300
JUANHercs <juan.hercs@gmail.com>
2021-05-07 16:04:48 +0000

    last_digit_serie_print

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        number * - 1
+        number = (number * - 1)
     lst = number % 10
     print("{}".format(lst), end='')
     return lst
