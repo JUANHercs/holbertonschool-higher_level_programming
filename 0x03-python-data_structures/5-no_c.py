7fbe8ae1dc41300831e03b46c48c78656d896fbf
JUANHercs <juan.hercs@gmail.com>
2021-05-10 05:26:45 +0000

    creat_new_fullfill

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new2 = ""
-    new = list(my_string)
-    if 'c' in new:
-        new.remove('c')
-    if 'C' in new:
-        new.remove('C')
-    for i in new:
-        new2 += i
-    return new2
+    new = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new += i
+    return new
