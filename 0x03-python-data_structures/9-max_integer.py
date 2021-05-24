60c3fe55309669b5553fc712a0722ac556adaa1c
“JUANHercs” <juan.hercs@gmail.com>
2021-05-24 13:04:04 -0500

    better_way_with_sort

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
+''' Better way to do it '''
+
+
 def max_integer(my_list=[]):
-    if my_list is None:
+    if len(my_list) == 0:
+        return None
+    my_list.sort()
+    return my_list[-1]
+
+''' first attempt 
+
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) is 0:
         return None
     max = -10000
     for element in my_list:
         if element > max:
             max = element
     return (max)
+    '''
