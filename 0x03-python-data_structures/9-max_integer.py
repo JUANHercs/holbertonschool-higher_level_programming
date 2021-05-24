6d71e7a09d9344f4ad2021fa562dc49c24c02935
“JUANHercs” <juan.hercs@gmail.com>
2021-05-24 12:51:46 -0500

    max_number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/user/bin/python3
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    max = -10000
+    for element in my_list:
+        if element > max:
+            max = element
+    return (max)
