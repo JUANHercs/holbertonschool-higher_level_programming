ff7485f8084c86e80f50936a1958577fec11def4
“JUANHercs” <juan.hercs@gmail.com>
2021-06-01 22:40:42 -0500

    fix_empty_line

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,6 +9,7 @@ def is_same_class(obj, a_class):
     method check if is same  class
     '''
     if type(obj) == a_class:
+        '''proper way isinstance(obj, a_class)'''
         return True
     else:
         return False
