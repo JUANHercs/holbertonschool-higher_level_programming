045bb067384a25308df2da939b9c849a742dc1dd
“JUANHercs” <juan.hercs@gmail.com>
2021-06-01 22:28:12 -0500

    class_instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+checkif the object its exactly an instance
+of specific class
+'''
+
+def is_same_class(obj, a_class):
+    '''
+    method check if is same  class
+    '''
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
