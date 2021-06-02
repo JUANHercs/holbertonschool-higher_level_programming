99d65cfd73e2ba9e5548345b83e339a716657ba5
“JUANHercs” <juan.hercs@gmail.com>
2021-06-01 23:44:50 -0500

    raise_error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Geometry figure module
+Class: BaseGeometry
+Method: Area()
+"""
+
+
+class BaseGeometry:
+    """
+    the class inherits
+    """
+    def Area(self):
+        """ Raise error """
+        raise Exception("area() is not implemented")
