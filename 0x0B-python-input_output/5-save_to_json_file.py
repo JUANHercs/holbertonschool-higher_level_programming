b73858cf2eecb1789f5040f4592891e0f9c8317e
“JUANHercs” <juan.hercs@gmail.com>
2021-06-03 20:02:07 -0500

    fix_pep8

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,5 +14,5 @@ def save_to_json_file(my_obj, filename):
     return number of characters written
     """
     with open(filename, 'w', encoding='utf8') as file_with_json:
-        #file_with_json.write(json.dumps(my_obj))
+        # file_with_json.write(json.dumps(my_obj))
         json.dump(my_obj, file_with_json)
