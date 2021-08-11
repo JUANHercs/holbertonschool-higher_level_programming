550b2acfa17edc0e1d49a0226d39701d56b711f0
JUANHercs <juan.hercs@gmail.com>
2021-08-11 06:16:02 +0000

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
 """
-Display all values in the states table of database hbtn_0e_0_usa
-where name matches
+    Displays all values in the states table where name matches the argument
 """
-
-import MySQLdb
-from sys import argv
-
 if __name__ == "__main__":
     """ the file is not executed when imported"""
+    # Connect with database
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         database=sys.argv[3])
 
-    #Connect with database
-    db = MySQLdb.connect(host = 'localhost',
-                         user = sys.argv[1],
-                         passwd = sys.argv[2],
-                         database = sys.argv[3])
-    #create cursor
     cursor = db.cursor()
-    #execute the query
-    cursor.execute("SELECT * FROM states"
-                   "WHERE name LIKE BINARY '{}'".format(sys.argv[4]))
-    #Selecting the information
+    cursor.execute("SELECT * FROM states "
+                   "WHERE name LIKE BINARY '{:s}' "
+                   "ORDER BY id ASC".format(sys.argv[4]))
+
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-    #closing cursor and database
     cursor.close()
     db.close()
